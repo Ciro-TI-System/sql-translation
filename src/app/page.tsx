8a20d57342bc24778a8b3ab1054942f6dc9ae8e4
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2023-08-30 23:18:05 -0300

    Applying styling with Tailwind.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Trash2, Stars } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="max-w-[430px] mx-auto pt-12 pb-4">
+    <div className="max-w-[430px] mx-auto px-4 pt-12 pb-4">
       <header className="flex items-center justify-between">
         <svg
           width="118"
@@ -45,22 +45,42 @@ export default function Home() {
         </button>
       </header>
 
-      <form>
-        <label htmlFor="schema">Cole seu código SQL aqui:</label>
-        <textarea name="schema" id="schema" />
+      <form className="py-8 w-full flex flex-col text-foam">
+        <label htmlFor="schema" className="text-lg font-light">
+          Cole seu código SQL aqui:
+        </label>
+        <textarea
+          className="my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-lime-600"
+          name="schema"
+          id="schema"
+        />
 
-        <label htmlFor="question">Faça uma pergunta sobre o código:</label>
-        <textarea name="question" id="question" />
+        <label htmlFor="question" className="text-lg font-light">
+          Faça uma pergunta sobre o código:
+        </label>
+        <textarea
+          className="my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-lime-600"
+          name="question"
+          id="question"
+        />
 
-        <button type="submit" className="text-pistachio">
+        <button
+          type="submit"
+          className="text-pistachio flex items-center justify-center rounded-lg border border-pistachio h-14 gap-2"
+        >
           <Stars className="w-6 h-6" />
           Perguntar à inteligência artificial
         </button>
       </form>
 
-      <div>
-        <span>Resposta:</span>
-        <textarea />
+      <div className="mt-6">
+        <span className="text-lg font-light text-foam">
+          Resposta:
+        </span>
+        <textarea
+          readOnly
+          className="my-4 w-full bg-transparent border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-lime-600"
+        />
       </div>
     </div>
   );
